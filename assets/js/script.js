3e97c8ecf4b25aa6247394392330ca93b129d410
Developer Sobuj <82040377+so8uj@users.noreply.github.com>
2025-03-28 10:03:59 +0600

    All  page con contain the submit form button
    FIxed a minor js error. All page not has the submit form button. so make some adjustment with condition if there is submit form button in dom then run the click event otherwise skip it.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,27 @@
 var submit_form = document.querySelector('#submit-form');
-submit_form.addEventListener("click", function () {
-
-    let all_inputs = document.querySelectorAll(".input");
-    let validation = [];
-
-    all_inputs.forEach(input => {
-        if (input.value === "") {
-            let data_name = input.getAttribute("data-name");
-            validation.push(`${data_name} Field is Required!`);
-        }
-    });
-    let parant_form = submit_form.closest('form');
-    if (validation.length > 0) {
-        let validation_errors = document.querySelector(".validation-errors");
-        validation_errors.classList.add('invalid');
-        validation_errors.innerHTML = '';
-        validation.forEach(error => {
-            validation_errors.innerHTML += `<p>${error}</p>`;
+if(submit_form){
+    submit_form.addEventListener("click", function () {
+    
+        let all_inputs = document.querySelectorAll(".input");
+        let validation = [];
+    
+        all_inputs.forEach(input => {
+            if (input.value === "") {
+                let data_name = input.getAttribute("data-name");
+                validation.push(`${data_name} Field is Required!`);
+            }
         });
-    } else {
-        parant_form.submit();
-    }
-
-});
+        let parant_form = submit_form.closest('form');
+        if (validation.length > 0) {
+            let validation_errors = document.querySelector(".validation-errors");
+            validation_errors.classList.add('invalid');
+            validation_errors.innerHTML = '';
+            validation.forEach(error => {
+                validation_errors.innerHTML += `<p>${error}</p>`;
+            });
+        } else {
+            parant_form.submit();
+        }
+    
+    });
+}
